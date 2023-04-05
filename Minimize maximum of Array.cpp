b839d4f6a40ca0c082fc631a183b35f2161fd929
Harshdeep Yadav <harshdeepy5708@gmail.com>
2023-04-06 00:09:38 +0530

    Solved

---
+++ Minimize maximum of Array.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int minimizeArrayValue(vector<int>& nums) {
+        int n=nums.size();
+        long long mini=nums[0];
+        long long sum=nums[0];
+        
+        for (int i = 1; i<n; i++){
+            sum += nums[i];
+            if (nums[i] > mini){
+                int x = sum/(i+1);
+                if (x >= mini){
+                    if (sum%(i+1))
+                        mini = x+1;
+                    else
+                        mini =x;
+                }
+            }
+        }
+        return mini;
+    }
+};
+
+
