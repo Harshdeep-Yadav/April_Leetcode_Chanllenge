fcb724e4f963abaa2f498028d275016f7dd37680
Harshdeep Yadav <harshdeepy5708@gmail.com>
2023-04-14 03:08:15 +0530

    Solved

---
+++ Validate Stack Sequences.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool validateStackSequences(vector<int>& pushed, vector<int>& popped) {
+        int n = pushed.size();
+        stack<int>st;
+        for(int i=0,j=0;i<n;i++){
+            st.push(pushed[i]);
+            while(!st.empty() && st.top()==popped[j]){
+                st.pop();
+                j++;
+            }
+        }
+        return st.empty();// ->ture is hai  // if empty to true warna false;
+    }
+};
